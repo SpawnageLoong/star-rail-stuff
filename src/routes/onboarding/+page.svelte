713d636c2156ab8ddef70181b089d2c39396b2a0
Richard Loong <richardloongcj@gmail.com>
2023-07-03 16:31:12 +0800

    Add skeleton onboarding pages

---
+++ src/routes/onboarding/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import methodSelected from './+layout.svelte';
+  import autoOnboard from './+layout.svelte';
+</script>
+
+<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
+<div class="flex flex-col items-center">
+  <a
+    href="onboarding/manual"
+    class="btn btn-primary">
+    Manual Onboarding
+  </a>
+  <a
+    href="onboarding/auto"
+    class="btn btn-primary">
+    Automatic Onboarding
+  </a>
+</div>
