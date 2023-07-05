7db521f85c58f74f023aa65e5918a2a09cdd8e28
SpawnageLoong <richardloongcj@gmail.com>
2023-07-05 14:21:53 +0800

    Add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
   import Navbar from "$lib/components/Navbar.svelte";
   import Footer from "$lib/components/Footer.svelte";
   import Analytics from "$lib/components/Analytics.svelte";
+  
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 
