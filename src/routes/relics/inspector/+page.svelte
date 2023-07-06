37f691d29d0be8b6eec5a2da38ce1eae359b1f6e
Richard Loong <richardloongcj@gmail.com>
2023-07-06 17:12:08 +0800

    Add basic cloud saves

--- src/routes/relics/inspector/+page.svelte
+++ src/routes/relics/inspector/+page.svelte
@@ -1,9 +1,43 @@
 <script lang="ts">
   import '$lib/types.d.ts'
+  import MainStatInspect from "$lib/components/relics/mainStatInspect.svelte";
   import SubstatInspect from "$lib/components/relics/substatInspect.svelte";
   import { customRelicStore as relicStore } from "$lib/components/relics/relicStore";
-	import MainStatInspect from "$lib/components/relics/mainStatInspect.svelte";
+	
+  import { db, user } from "$lib/firebase";
+  import { doc, getDoc, writeBatch } from "firebase/firestore";
 
+  let debounceTimer: NodeJS.Timeout | null = null;
+
+  async function saveRelic() {
+    window.alert("Saving...");
+    if ($user === null) {
+      window.alert("You must be logged in to save relics.");
+      return
+    };
+    const batch = writeBatch(db);
+    batch.set(doc(db, "users", $user!.uid), {
+      uid: $user?.uid,
+      set: $relicStore.setID,
+      piece: $relicStore.pieceID,
+      level: $relicStore.relicLevel,
+      mainStat: $relicStore.mainStatID,
+      substatIDs: $relicStore.substatIDs.map((substat) => substat),
+      substatValues: $relicStore.substatValues.map((substat) => substat)
+    });
+    await batch.commit();
+    window.alert("Saved!");
+  }
+
+  async function saveRelicAsNew() {
+    // TODO
+  }
+  async function loadRelicList() {
+    // TODO
+  }
+  async function loadRelic() {
+    // TODO
+  }
 </script>
 
 <svelte:head>
@@ -14,8 +48,14 @@
   <h1 class="text-3xl">Substat Inspector</h1>
 
   <div class="flex flex-row gap-4">
-    <div>
-      <MainStatInspect relicStore={relicStore}/>
+    <div class="flex flex-col gap-4">
+      <div>
+        <h2 class="text-2xl text-center">Image goes here</h2>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={saveRelic}>Save</button>
+      </div>
+      <div>
+        <MainStatInspect relicStore={relicStore}/>
+      </div>
     </div>
 
     <div class="flex flex-col gap-4">
