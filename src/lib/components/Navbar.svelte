2a3ec65bb125afc60a909a5cded08547c7767340
SpawnageLoong <richardloongcj@gmail.com>
2023-07-09 13:15:51 +0800

    Update UI and redirects

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -11,7 +11,7 @@
 <div class="navbar bg-base-100 sticky top-0 gap-2 z-10">
   <div class="flex-1">
     <a
-      href="/home"  
+      href="/"  
       class="btn btn-ghost normal-case text-xl">
       Star Rail Stuff
       </a>
@@ -25,13 +25,7 @@
     </span>
     <ul tabindex="-1" class="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-200 rounded-box w-52">
       <li>
-        <a href="/relics/info">Info</a>
-      </li>
-      <li>
         <a href="/relics/inspector">Inspector</a>
-      </li>
-      <li>
-        <a href="/relics/builder">Builder</a>
       </li>
     </ul>
   </div>
@@ -44,15 +38,6 @@
         </div>
       </span>
       <ul tabindex="-1" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-200 rounded-box w-52">
-        <li>
-          <a
-            href="/profile"
-            class="justify-between">
-            Profile
-            <span class="badge">New</span>
-          </a>
-        </li>
-        <li><a href="/settings">Settings</a></li>
         <li>
           <btn
             on:click={signOutGoogle}
