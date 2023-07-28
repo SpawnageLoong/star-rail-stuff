f8cefe58258b21db6f338882059e0d21d9f9b766
SpawnageLoong <richardloongcj@gmail.com>
2023-07-28 20:32:00 +0800

    Add main stat viewer

--- src/lib/components/relics/inspectorCard.svelte
+++ src/lib/components/relics/inspectorCard.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
   import '$lib/types.d.ts'
   import { customRelicStore as relicStore } from "$lib/components/relics/relicStore";
-  import { setMetadata } from './relicData';
+  import { mainStatIcons, setMetadata, mainStatMetadata } from './relicData';
   import SetSelector from './setSelector.svelte';
   import PieceSelector from './pieceSelector.svelte';
   import MainStatInspect from './mainStatInspect.svelte';
   import SubstatInspect from './substatInspect.svelte';
+
+  let mainStatID: number = 0;
+  let relicLevel: number = 0;
+  let float: boolean = false;
+  let mainStatValue: number = 0;
+
+  $: mainStatValue = mainStatMetadata[mainStatID].min + (mainStatMetadata[mainStatID].max - mainStatMetadata[mainStatID].min) * (relicLevel / 15);
+
+  relicStore.subscribe(( relic: customRelic) => {
+    mainStatID = relic.mainStatID;
+    relicLevel = relic.relicLevel;
+    float = mainStatMetadata[mainStatID].float;
+  })
 </script>
 
 <div class="w-[744px] mt-4 bg-surfaceContainerHigh rounded-xl shadow-xl">
@@ -16,11 +29,19 @@
         src={setMetadata[$relicStore.setID].pieces[$relicStore.pieceID].img}
         alt="Relic"/>
       <div class="">
-        <div class="h-">
+        <div class="h-8">
           <!-- Relic Chips go here -->
         </div>
-        <p class="text-white headline-medium w-[440px]">{setMetadata[$relicStore.setID].pieces[$relicStore.pieceID].name}</p>
-        <p class="text-white headline-medium">+{$relicStore.relicLevel}</p>
+        <div class="text-white headline-medium w-[440px] h-9 flex items-center">{setMetadata[$relicStore.setID].pieces[$relicStore.pieceID].name}</div>
+        <div class="text-white headline-medium h-9 flex items-center">+{$relicStore.relicLevel}</div>
+        <div class="flex flex-row gap-2 items-center">
+          <img
+            class="w-12 h-12"
+            src={mainStatIcons[$relicStore.mainStatID]}
+            alt="Main Stat"/>
+          <p class="text-white display-small h-[44px] relative -top-[4px]">{mainStatMetadata[$relicStore.mainStatID].name}</p>
+        </div> 
+        <div class="text-white display-medium h-[52px] flex items-center">{float ? mainStatValue.toFixed(1)+"%" : Math.round(mainStatValue)}</div> 
       </div>
     </div>
   </div>
