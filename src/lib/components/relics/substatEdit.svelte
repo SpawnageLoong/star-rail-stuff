00688ab4d8be8cafa02629b2e0d053c2c06651c9
SpawnageLoong <richardloongcj@gmail.com>
2023-08-01 00:25:38 +0800

    Add dropdown menu

--- src/lib/components/relics/substatEdit.svelte
+++ src/lib/components/relics/substatEdit.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { substatMetadata } from "$lib/components/relics/relicData";
   import { customRelicStore as relicStore } from "$lib/components/relics/relicStore";
+  import { fade } from "svelte/transition";
 
   export let substat:number = 0;
   let substatDropdown:boolean = false;
@@ -21,7 +22,21 @@
   {substatMetadata[$relicStore.substatIDs[substat]].name}
   </button>
   {#if substatDropdown}
-    <div class="absolute top-5 bg-primary rounded-lg z-10">absolute</div>
+    <div
+      class="absolute top-12 z-[1] p-2 shadow bg-slate-700 rounded-lg w-52"
+      transition:fade={{duration:200}}>
+      {#each substatMetadata as substatMeta, i}
+        <button
+          class="text-white w-full text-left p-2 rounded-lg hover:bg-slate-600"
+          on:click={() => {
+            relicStore.setSubstatID( substat, i );
+            relicStore.setSubstatValue( substat, substatMeta.min );
+            closeSubstatDropdown();
+          }}>
+          {substatMeta.name}
+        </button>
+      {/each}
+    </div>
   {/if}
 </div>
 
