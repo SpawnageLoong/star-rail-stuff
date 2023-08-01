1fbd2e3f512da331b4e45b08d6b3194bf93b23eb
SpawnageLoong <richardloongcj@gmail.com>
2023-08-01 10:14:52 +0800

    Separate dropdown into its own component

--- src/lib/components/relics/substatEdit.svelte
+++ src/lib/components/relics/substatEdit.svelte
@@ -1,43 +1,6 @@
 <script lang="ts">
-  import { substatMetadata } from "$lib/components/relics/relicData";
-  import { customRelicStore as relicStore } from "$lib/components/relics/relicStore";
-  import { fade } from "svelte/transition";
-
+	import SubstatDropdown from "./substatDropdown.svelte";
   export let substat:number = 0;
-  let substatDropdown:boolean = false;
-
-  function openSubstatDropdown() {
-    substatDropdown = true;
-    document.body.addEventListener("click", closeSubstatDropdown);
-  }
-  function closeSubstatDropdown() {
-    substatDropdown = false;
-  }
 </script>
 
-<div class="relative">
-  <button
-    class="bg-secondaryContainer rounded-lg text-white p-2"
-    on:click|stopPropagation={openSubstatDropdown}>
-  {substatMetadata[$relicStore.substatIDs[substat]].name}
-  </button>
-  {#if substatDropdown}
-    <div
-      class="absolute top-12 z-[1] p-2 shadow bg-slate-700 rounded-lg w-52"
-      transition:fade={{duration:200}}>
-      {#each substatMetadata as substatMeta, i}
-        <button
-          class="text-white w-full text-left p-2 rounded-lg hover:bg-slate-600"
-          on:click={() => {
-            relicStore.setSubstatID( substat, i );
-            relicStore.setSubstatValue( substat, substatMeta.min );
-            closeSubstatDropdown();
-          }}>
-          {substatMeta.name}
-        </button>
-      {/each}
-    </div>
-  {/if}
-</div>
-
-
+<SubstatDropdown substat={substat}/>
