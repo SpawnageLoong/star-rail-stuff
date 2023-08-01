a9b45adf820c8719f22f935e3962040a5f23b6db
SpawnageLoong <richardloongcj@gmail.com>
2023-08-01 13:44:36 +0800

    UI overhaul completed

--- src/lib/components/relics/relicEdit/inspectorCardEdit.svelte
+++ src/lib/components/relics/relicEdit/inspectorCardEdit.svelte
@@ -7,15 +7,13 @@
 
   let mainStatID: number = 0;
   let relicLevel: number = 0;
-  let float: boolean = false;
-  let mainStatValue: number = 0;
+  export let modalOpen: boolean = false;
 
   $: mainStatValue = mainStatMetadata[mainStatID].min + (mainStatMetadata[mainStatID].max - mainStatMetadata[mainStatID].min) * (relicLevel / 15);
 
   relicStore.subscribe(( relic: customRelic) => {
     mainStatID = relic.mainStatID;
     relicLevel = relic.relicLevel;
-    float = mainStatMetadata[mainStatID].float;
   })
 
   let setDropdownOpen: boolean = false;
@@ -28,6 +26,11 @@
   <div class="flex flex-col gap-4">
     <div class="flex flex-row">
       <div class="flex grow text-white headline-medium">Relic Editor</div>
+      <button
+        class="text-white bg-secondaryContainer hover:bg-primaryContainer font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+        on:click={() => {modalOpen = false}}>
+        Close
+      </button>
     </div>
     <div class="grid grid-cols-2 grid-rows-1 gap-4">
       <div>
--- src/routes/relics/inspector/+page.svelte
+++ src/routes/relics/inspector/+page.svelte
@@ -9,6 +9,7 @@
 	import EditFab from '$lib/components/EditFab.svelte';
 	import DeleteFab from '$lib/components/DeleteFab.svelte';
 	import SaveFab from '$lib/components/SaveFab.svelte';
+	import InspectorCardEdit from '$lib/components/relics/relicEdit/inspectorCardEdit.svelte';
 
   let relicID: string = '';
   let relicNick: string = '';
@@ -43,19 +44,11 @@
 <div class="flex flex-row pt-4 ml-[365px] place-content-center gap-4">
   <InspectorCard />
   <div class="flex flex-col gap-4">
-    {#if editMode}
-      <button
-        class=""
-        on:click={() => {editMode = false}}>
-        <SaveFab />
-      </button>
-    {:else}
-      <button
-        class=""
-        on:click={() => {editMode = true}}>
-        <EditFab />
-      </button>
-    {/if}
+    <button
+      class=""
+      on:click={() => {editMode = true}}>
+      <EditFab />
+    </button>
     <button
       class=""
       on:click={deleteRelic}>
@@ -63,3 +56,38 @@
     </button>
   </div>
 </div>
+
+{#if editMode}
+  <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <!--
+      Background backdrop, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+    -->
+    <div class="fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm transition-opacity"></div>
+
+    <div class="fixed inset-0 z-10 overflow-y-auto">
+      <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <!--
+          Modal panel, show/hide based on modal state.
+
+          Entering: "ease-out duration-300"
+            From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            To: "opacity-100 translate-y-0 sm:scale-100"
+          Leaving: "ease-in duration-200"
+            From: "opacity-100 translate-y-0 sm:scale-100"
+            To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        -->
+        <div class="relative ml-[365px] -top-40 transform overflow-auto rounded-lg shadow-xl transition-all">
+          <InspectorCardEdit bind:modalOpen={editMode}/>
+        </div>
+      </div>
+    </div>
+  </div>
+{/if}
+
